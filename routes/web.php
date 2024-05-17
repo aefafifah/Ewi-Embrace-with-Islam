d82bc058ea72b52c766db4bd20574a4444b75ea1
aefafifah <aefgoescollege@gmail.com>
2024-05-17 21:55:34 +0700

    add route hadist

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuranController;
+use App\Http\Controllers\HadistController;
 
 Route::get('/', function () {
     return view('home');
@@ -11,3 +12,6 @@ Route::get('/', function () {
 
 Route::get('quran',[QuranController::class, 'index']);
 Route::get('quran/{id}',[QuranController::class, 'indexId']);
+
+//Hadist
+Route::get('hadist',[HadistController::class, 'index']);
