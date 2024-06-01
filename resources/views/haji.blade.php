e00d7b176dbf062510c933f006fd368d7d2f3b67
Hammam Karim <hammmamjauharul@gmail.com>
2024-06-01 07:11:20 +0700

    Update 8_3 Islamic Solution

--- resources/views/haji.blade.php
+++ resources/views/haji.blade.php
@@ -15,25 +15,119 @@
 
     </section>
 
-    <!-- dalil -->
+
+    <!-- dalil al-quran-->
     <section class="dalil">
 
+        <h2>Dalil Al-Qur'an & Tafsirnya</h2>
+        <hr>
+
         <div class="dalil-container">
+            <h3 class="dalil-title">~ Surah Al-Baqarah, Ayat 189</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p> يَسْـَٔلُونَكَ عَنِ ٱلْأَهِلَّةِ ۖ قُلْ هِىَ مَوَٰقِيتُ لِلنَّاسِ وَٱلْحَجِّ ۗ وَلَيْسَ ٱلْبِرُّ
+                        بِأَن تَأْتُوا۟ ٱلْبُيُوتَ مِن ظُهُورِهَا وَلَـٰكِنَّ ٱلْبِرَّ مَنِ ٱتَّقَىٰ ۗ وَأْتُوا۟ ٱلْبُيُوتَ
+                        مِنْ أَبْوَٰبِهَا ۚ وَٱتَّقُوا۟ ٱللَّهَ لَعَلَّكُمْ تُفْلِحُونَ (١٨٩)</p>
+                </div>
+                <p class="dalil-translation">Mereka bertanya kepadamu (Muhammad) tentang bulan sabit. Katakanlah, "itu
+                    adalah (penunjuk) waktu bagi manusia dan (ibadah) haji." Dan bukanlah suatu kebajikan memasuki rumah
+                    dari atasnya, tetapi kebajikan adalah (kebajikan) orang yang bertakwa. Masukilah rumah-rumah dari
+                    pintu-pintunya, dan bertakwalah kepada Allah agar kamu beruntung.</p>
+                <button class="tafsir-button" data-title="Tafsir Muyassar Surah Al-Baqarah, Ayat 189"
+                    data-text="Wahai nabi, sahabat-sahabatmu bertanya kepadamu tentang Hilal dan perubahan bentuknya. katakanlah kepada mereka,' Allah menjadikan hilal sebagai tanda-tanda bagi manusia untuk mengetahui waktu-waktu ibadah mereka yang telah ditentukan, waktu puasa dan haji serta batas tempo transaksi-transaksi mereka. Bukan termasuk kebajikan, kebiasaan yang kalian lakukan di masa jahiliyah dan permulaan Islam dengan masuk rumah-rumah melalui bagian belakangnya jika kalian memulai ihram untuk Haji atau umrah, lantaran mengira bahwa perbuatan itu adalah bentuk pendekatan diri kepada Allah. Akan tetapi, kebajikan yang sebenarnya adalah perbuatan orang yang bertakwa kepada Allah dan menjauhi maksiat maksiat. dan masukilah rumah-rumah melalui pintu-pintunya ketika kalian berihram untuk Haji atau umrah, dan takutlah kepada Allah dalam seluruh urusan kalian, supaya kalian beruntung menggapai semua yang kalian sukai dari kebaikan di dunia dan akhirat.'">Tafsir</button>
+                <button class="tatbhiq-button" data-title="Tatbhiq Surah Al-Baqarah, Ayat 189"
+                    data-text="Faedah yang dapat diambil dari ayat ini berdasarkan Tafsir As Sa'di sebagai berikut:
+
+                    Hendaknya seseorang bertanya mengenai perkara yang bermanfaat untuknya, serta meninggalkan pertanyaan mengenai perkara yang tidak bermanfaat untuk dirinya.
+                    Manfaat bulan qamariyah amat besar, yaitu untuk mengetahui berbagai waktu ibadah.
+                    Keharaman membuat perkara baru dalam agama (bid’ah), walaupun dengan niat untuk melakukan ketaatan kepada Allah ta’ala dan mendapat pahala.
+                    Perintah untuk bertakwa berimplikasi pada kebahagiaan dan keselamatan hamba di dunia dan akhirat.
+                    ">Tatbhiq</button>
+            </div>
+
+            <h3 class="dalil-title">~ Surah Al-Baqarah, Ayat 197</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p>ٱلْحَجُّ أَشْهُرٌۭ مَّعْلُومَـٰتٌۭ ۚ فَمَن فَرَضَ فِيهِنَّ ٱلْحَجَّ فَلَا رَفَثَ وَلَا فُسُوقَ وَلَا
+                        جِدَالَ فِى ٱلْحَجِّ ۗ وَمَا تَفْعَلُوا۟ مِنْ خَيْرٍۢ يَعْلَمْهُ ٱللَّهُ ۗ وَتَزَوَّدُوا۟ فَإِنَّ
+                        خَيْرَ ٱلزَّادِ ٱلتَّقْوَىٰ ۚ وَٱتَّقُونِ يَـٰٓأُو۟لِى ٱلْأَلْبَـٰبِ (١٩٧)</p>
+                </div>
+                <p class="dalil-translation">(Musim) haji itu (pada) bulan-bulan yang telah dimaklumi. Barang siapa
+                    mengerjakan (ibadah) haji dalam (bulan-bulan) itu, maka janganlah dia berkata jorok (rafaṡ), berbuat
+                    maksiat dan bertengkar dalam (melakukan ibadah) haji. Segala yang baik yang kamu kerjakan, Allah
+                    mengetahuinya. Bawalah bekal, karena sesungguhnya sebaik-baik bekal adalah takwa. Dan bertakwalah
+                    kepada-Ku Wahai orang-orang yang mempunyai akal sehat!</p>
+                <button class="tafsir-button" data-title="Tafsir Muyassar Surah Al-Baqarah, Ayat 197"
+                    data-text="Waktu pelaksanaan haji itu adalah pada bulan-bulan yang telah dimaklumi, yaitu bulan Syawal, dzulqo'dah, dan 10 hari dari bulan Dzulhijjah. Maka barangsiapa telah memantapkan niat Haji atas dirinya pada bulan-bulan tersebut dengan memasuki keadaan ihram, maka diharamkan atas dirinya untuk berjimak dan aktivitas-aktivitas pengantarannya, baik berbentuk perkataan maupun perbuatan. Dan haram atas dirinya keluar dari ketaatan kepada Allah dengan berbuat maksiat-maksiat, dan perdebatan dalam berhaji yang dapat menyeret padat tersulut nya kemarahan dan kebencian. Dan apapun kebaikan yang kalian perbuat niscaya Allah mengetahuinya, lalu membalasi tiap-tiap orang yang sesuai dengan amal perbuatannya. Dan bawalah bagi kalian perbekalan dari jenis makanan dan minuman bagi perjalanan ibadah haji, dan perbekalan dari jenis amal Shalih untuk Kampung akhirat. Karena sesungguhnya sebaik-baik perbekalan adalah ketaqwaan kepada Allah. dan takutlah kepadaku Wahai orang-orang yang berakal sehat.">Tafsir</button>
+                <button class="tatbhiq-button" data-title="Tatbhiq Surah Al-Baqarah, Ayat 197"
+                    data-text="Faedah yang dapat diambil dari ayat ini berdasarkan Tafsir As Sa'di sebagai berikut:
+
+                    Keharaman melakukan perbuatan keji (rafats), kefasiqan, dan permusuhan tatkala sedang ihram.
+                    Dan disunnahkan bagi jama’ah haji untuk memperbanyak amalan kebaikan selama ibadah hajinya agar pahalanya bertambah dan mabrur hajinya.">Tatbhiq</button>
+            </div>
+
+            <h3 class="dalil-title">~ Surah Ali 'Imran', Ayat 97</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p>فِيهِ ءايَـٰتٌۢ بَيِّنَـٰتٌۭ مَّقَامُ إِبْرَٰهِيمَ ۖ وَمَن دَخَلَهُۥ كَانَ ءامِنًۭا ۗ وَلِلَّهِ عَلَى
+                        ٱلنَّاسِ حِجُّ ٱلْبَيْتِ مَنِ ٱسْتَطَاعَ إِلَيْهِ سَبِيلًۭا ۚ وَمَن كَفَرَ فَإِنَّ ٱللَّهَ غَنِىٌّ
+                        عَنِ ٱلْعَـٰلَمِينَ (٩٧)</p>
+                </div>
+                <p class="dalil-translation">Di sana terdapat tanda-tanda yang jelas, (di antaranya) maqam Ibrahim. Barang
+                    siapa memasukinya (Baitullah) amanlah dia. Dan (di antara) kewajiban manusia terhadap Allah adalah
+                    melaksanakan ibadah haji ke Baitullah, yaitu bagi orang-orang yang mampu mengadakan perjalanan ke sana.
+                    Barang siapa mengingkari (kewajiban) haji, maka ketahuilah bahwa Allah Mahakaya (tidak memerlukan
+                    sesuatu) dari seluruh alam.</p>
+                <div class="button-container">
+                    <button class="tafsir-button" data-title="Tafsir Jalalain Surah Ali 'Imran', Ayat 97"
+                        data-text="(Padanya terdapat tanda-tanda yang nyata) di antaranya (makam Ibrahim) yakni batu tempat berpijaknya Ibrahim sewaktu mendirikan Baitullah itu. Kedua telapak kakinya meninggalkan bekas padanya sampai sekarang dan tetap sepanjang zaman walaupun pemerintahan yang berkuasa sudah silih berganti. Di antaranya pula dilipatgandakannya pahala kebaikan bagi yang salat di dalamnya dan burung tidak dapat terbang di atas Kakbah (dan barang siapa memasukinya menjadi amanlah dia) artinya bebas dari ancaman pembunuhan, keaniayaan dan lain-lain. (Mengerjakan haji di Baitullah itu menjadi kewajiban manusia terhadap Allah ﷻ) Ada yang membaca hajja dengan makna menyengaja. Lalu sebagai badal dari 'manusia' ialah (yakni orang-orang yang sanggup mengadakan perjalanan kepadanya) yang oleh Nabi ﷺ. ditafsirkan dengan adanya perbekalan dan kendaraan, menurut riwayat Hakim dan lain-lain. (Barang siapa yang kafir) terhadap Allah ﷻ atau terhadap kewajiban haji (maka sesungguhnya Allah ﷻ Maha Kaya terhadap seluruh alam) artinya tidak memerlukan manusia, jin dan malaikat serta amal ibadah mereka.">Tafsir</button>
+                    <button class="tatbhiq-button" data-title="Tatbhiq Surah Ali 'Imran', Ayat 97"
+                        data-text="Faedah yang dapat diambil dari ayat ini berdasarkan Tafsir As Sa'di sebagai berikut:
+
+                        Termasuk tanda yang jelas juga adalah bahwa orang yang memasukinya (Baitullah) akan aman baik secara syara' maupun taqdir. Secara syara' adalah, bahwa Allah memerintahkan rasul-Nya untuk menghormatinya dan mengamankan orang yang memasukinya serta tidak boleh diserang, bahkan sampai mengena pula kepada hewan buruannya, pepohonan dan tumbuh-tumbuhan. Sebagian ulama ada yang berpendapat berdasarkan ayat ini, bahwa barang siapa yang melakukan tindak pidana di luar tanah haram, lalu ia berlindung ke baitullah, maka ia akan aman dan tidak ditegakkan had sampai ia keluar daripadanya. Adapun aman secara taqdir adalah, bahwa Allah Subhaanahu wa Ta'aala dengan taqdir-Nya menetapkan dalam diri manusia, termasuk orang-orang kafir dan musyrik untuk menghormatinya. Lebih dari itu, orang yang berniat jahat terhadap Baitullah, Allah memberikan hukuman segera kepadanya sebagaimana yang terjadi pada As-habul Fiil (tentara bergajah yang hendak menghancurkan ka'bah).
+                        Sebelum menyebutkan kewajiban haji, Allah Subhaanahu wa Ta'aala menyebutkan kelebihan-kelebihan Baitullah yang menjadikan hati manusia berkeinginan untuk pergi ke sana, kelebihan itu adalah:
+                        Pertama, sebagai rumah ibadah pertama di dunia.
+                        Kedua, mendapatkan keberkahan, di mana tidak ada rumah yang paling banyak berkahnya dan paling banyak manfaatnya bagi manusia dibanding Baitullah.
+                        Ketiga, sebagai petunjuk bagi manusia
+                        Keempat, terdapat tanda-tanda.
+                        Kelima, orang yang memasukinya akan aman.">Tatbhiq</button>
+                </div>
+            </div>
 
         </div>
 
     </section>
-    <!-- end of dalil container -->
+
+    <div id="popup-container">
+        <div id="popup-content">
+            <button id="close-popup">&times;</button>
+            <h1 id="popup-title"></h1>
+            <hr style="color: black; height: 3px; margin-bottom: 35px;">
+            <p id="popup-text"></p>
+        </div>
+    </div>
+    <!-- end of dalil al-quran section -->
+
 
     <!-- tafsir -->
     <section class="tafsir">
 
-        <div class="tafsi-container">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1"
+                d="M0,160L60,160C120,160,240,160,360,181.3C480,203,600,245,720,229.3C840,213,960,139,1080,112C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+            </path>
+        </svg>
+        <br><br><br><br>
+        <h2>Dalil Hadits & Tafsirnya</h2>
+        <hr>
+        <div class="tafsir-container">
 
         </div>
 
     </section>
     <!-- end of dalil container -->
+
 
     <!-- dalil -->
     <section class="video">
@@ -45,5 +139,5 @@
     </section>
     <!-- end of dalil container -->
 
-    <script src="script.js"></script>
+    <script src="{{ asset('js/isso.js') }}"></script>
 @endsection
