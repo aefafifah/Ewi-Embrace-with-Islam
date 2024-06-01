640e4ace26544ef3f5a0ab8192b411448490a4f1
Hammam Karim <hammmamjauharul@gmail.com>
2024-06-01 11:41:27 +0700

    Update 8_6 Islamic Solution

--- resources/views/ilmu.blade.php
+++ resources/views/ilmu.blade.php
@@ -15,25 +15,130 @@
 
     </section>
 
-    <!-- dalil -->
+
+    <!-- dalil al-quran-->
     <section class="dalil">
 
+        <h2>Dalil Al-Qur'an & Tafsirnya</h2>
+        <hr>
+
         <div class="dalil-container">
+            <h3 class="dalil-title">~ Surah Thaha, Ayat 114</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p>فَتَعَـٰلَى ٱللَّهُ ٱلْمَلِكُ ٱلْحَقُّ ۗ وَلَا تَعْجَلْ بِٱلْقُرْءَانِ مِن قَبْلِ أَن يُقْضَىٰٓ
+                        إِلَيْكَ وَحْيُهُۥ ۖ وَقُل رَّبِّ زِدْنِى عِلْمًۭا (١١٤)</p>
+                </div>
+                <p class="dalil-translation">Maka Mahatinggi Allah, Raja yang sebenar-benarnya. Dan janganlah engkau
+                    (Muhammad) tergesa-gesa (membaca) Alquran sebelum selesai diwahyukan kepadamu, dan katakanlah, "Ya
+                    Tuhanku, tambahkanlah ilmu kepadaku."</p>
+                <div class="button-container">
+                    <button class="tafsir-button" data-title="Tafsir Muyassar Surah Thaha, Ayat 114"
+                        data-text="Allah maha bersih, tinggi dan suci dari semua kekurangan, Dia Raja Yang kekuasaaNya mengalahkan semua penguasa dan tirani, Yang mengendalikan segala sesuatu, Yang Mahabenar, janjiNya benar, ancamanNya benar, dan tiap-tiap sesuatu dariNya adalah kebenaran. Dan janganlah kamu tergesa-gesa (wahai Rasul) untuk mendahului Jibril dalam menerima al-Qur’an sebelum ia tuntas darinya. Dan katakanlah, “Wahai Tuhanku, tambahkanlah kepadaku ilmu disamping ilmu yang telah Engkau ajarkan kepadaku.”">Tafsir</button>
+                    <button class="tatbhiq-button" data-title="Tatbhiq Surah Thaha, Ayat 114"
+                        data-text="Faedah yang dapat diambil dari ayat ini adalah sebagai berikut: Nabi Muhammad shallallahu alaihi wa sallam dilarang oleh Allah menirukan bacaan malaikat Jibril dengan membacanya kalimat demi kalimat sebelum malaikat Jibril selesai membacakannya, karena Allah menjamin untuk mengumpulkan Al Qur’an di dalam dadanya dan membacakannya. Kecintaan Beliau yang sempurna terhadap ilmu ditunjukkan oleh kegigihannya untuk segera menghafal wahyu tersebut. Oleh karena itu, Allah memerintahkan Beliau untuk meminta tambahan ilmu kepada-Nya. Karena ilmu adalah kebaikan, dan banyaknya kebaikan perlu dicari, dan hal itu berasal dari Allah, maka cara untuk memperolehnya adalah dengan bersungguh-sungguh, rindu kepada ilmu, meminta kepada Allah, meminta pertolongan-Nya serta merasa butuh kepada-Nya di setiap waktu.
+
+                        Dari ayat ini dapat diambil kesimpulan tentang adab mencari ilmu, yaitu bahwa orang yang mendengarkan ilmu sepatutnya bersabar dan tidak langsung mencatat sampai pengajar menyelesaikan kata-katanya yang masih berkaitan">Tatbhiq</button>
+                </div>
+            </div>
+
+            <h3 class="dalil-title">~ Surah Az-Zumar, Ayat 9</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p>أَمَّنْ هُوَ قَـٰنِتٌ ءَانَآءَ ٱلَّيْلِ سَاجِدًۭا وَقَآئِمًۭا يَحْذَرُ ٱلْـَٔاخِرَةَ وَيَرْجُوا۟
+                        رَحْمَةَ رَبِّهِۦ ۗ قُلْ هَلْ يَسْتَوِى ٱلَّذِينَ يَعْلَمُونَ وَٱلَّذِينَ لَا يَعْلَمُونَ ۗ إِنَّمَا
+                        يَتَذَكَّرُ أُو۟لُوا۟ ٱلْأَلْبَـٰبِ (٩)</p>
+                </div>
+                <p class="dalil-translation">(Apakah kamu orang musyrik yang lebih beruntung) ataukah orang yang beribadah
+                    pada waktu malam dengan sujud dan berdiri, karena takut kepada (azab) akhirat dan mengharapkan rahmat
+                    Tuhannya? Katakanlah, "Apakah sama orang-orang yang mengetahui dengan orang-orang yang tidak
+                    mengetahui?" Sebenarnya hanya orang yang berakal sehat yang dapat menerima pelajaran.</p>
+                <button class="tafsir-button" data-title="Tafsir Jalalain Surah Az-Zumar, Ayat 9"
+                    data-text="(Apakah orang) dibaca Amman, dan dapat dibaca Aman (yang beribadah) yang berdiri melakukan amal ketaatan, yakni salat (di waktu-waktu malam) di saat-saat malam hari (dengan sujud dan berdiri) dalam salat (sedangkan ia takut kepada hari akhirat) yakni takut akan azab pada hari itu (dan mengharapkan rahmat) yakni surga (Rabbnya) apakah dia sama dengan orang yang durhaka karena melakukan kekafiran atau perbuatan-perbuatan dosa lainnya. Menurut qiraat yang lain lafal Amman dibaca Am Man secara terpisah, dengan demikian berarti lafal Am bermakna Bal atau Hamzah Istifham (Katakanlah, 'Adakah
+                    sama orang-orang yang mengetahui dengan orang-orang yang tidak mengetahui?'') tentu saja tidak,
+                    perihalnya sama dengan perbedaan antara orang yang alim dan orang yang jahil. (Sesungguhnya orang yang
+                    dapat menerima pelajaran) artinya, man menerima nasihat (hanyalah orang-orang yang berakal) yakni
+                    orang-orang yang mempunyai pikiran. ">Tafsir</button>
+                <button class="tatbhiq-button" data-title="Tatbhiq Surah Az-Zumar, Ayat 9"
+                    data-text="Faedah yang dapat diambil dari ayat ini adalah sebagai berikut: Ayat ini membandingkan antara orang yang menjalankan ketaatan kepada Allah dengan orang yang tidak demikian, dan membandingkan antara orang yang berilmu dengan orang yang tidak berilmu, yaitu bahwa hal ini termasuk perkara yang jelas bagi akal dan diketahui secara yakin perbedaannya. Oleh karena itu, tidaklah sama antara orang yang berpaling dari ketaatan kepada Tuhannya dan mengikuti hawa nafsunya dengan orang yang menjalankan ketaatan, bahkan ketaatan yang dijalankannya adalah ketaatan yang paling utama, yaitu shalat dan di waktu yang utama, yaitu malam. Allah menyifati orang ini dengan banyak beramal dan menyifatinya dengan rasa takut dan harap, rasa takut masuk ke neraka karena dosa-dosa yang lalu yang telah dikerjakannya dan rasa berharap masuk ke surga karena amal yang dikerjakannya.
+                    ">Tatbhiq</button>
+            </div>
+
+            <h3 class="dalil-title">~ Surah Al-Mujadilah, Ayat 11</h3>
+            <div class="dalil-box">
+                <div class="dalil-content">
+                    <p>يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوٓا۟ إِذَا قِيلَ لَكُمْ تَفَسَّحُوا۟ فِى ٱلْمَجَـٰلِسِ فَٱفْسَحُوا۟
+                        يَفْسَحِ ٱللَّهُ لَكُمْ ۖ وَإِذَا قِيلَ ٱنشُزُوا۟ فَٱنشُزُوا۟ يَرْفَعِ ٱللَّهُ ٱلَّذِينَ ءَامَنُوا۟
+                        مِنكُمْ وَٱلَّذِينَ أُوتُوا۟ ٱلْعِلْمَ دَرَجَـٰتٍۢ ۚ وَٱللَّهُ بِمَا تَعْمَلُونَ خَبِيرٌۭ (١١)</p>
+                </div>
+                <p class="dalil-translation">Wahai orang-orang yang beriman! Apabila dikatakan kepadamu, "Berilah kelapangan
+                    di dalam majelis-majelis," maka lapangkanlah, niscaya Allah akan memberi kelapangan untukmu. Dan apabila
+                    dikatakan, "Berdirilah kamu," maka berdirilah, niscaya Allah akan mengangkat (derajat) orang-orang yang
+                    beriman di antaramu dan orang-orang yang diberi ilmu beberapa derajat. Dan Allah Mahateliti atas apa
+                    yang kamu kerjakan.</p>
+                <button class="tafsir-button" data-title="Tafsir Jalalain Surah Al-Mujadilah Ayat 11"
+                    data-text="(Hai orang-orang yang beriman, apabila dikatakan kepada kalian, Berlapang-lapanglah)
+                    berluas-luaslah (dalam majelis) yaitu majelis tempat Nabi ﷺ berada, dan majelis zikir sehingga
+                    orang-orang yang datang kepada kalian dapat tempat duduk. Menurut suatu qiraat lafal al-majaalis dibaca
+                    al-majlis dalam bentuk mufrad (maka lapangkanlah, niscaya Allah akan memberi kelapangan untuk kalian) di
+                    surga nanti. (Dan apabila dikatakan, Berdirilah kalian ) untuk melakukan salat dan hal-hal lainnya
+                    yang termasuk amal-amal kebaikan (maka berdirilah) menurut qiraat lainnya kedua-duanya dibaca fansyuzuu
+                    dengan memakai harakat damah pada huruf Syinnya (niscaya Allah akan meninggikan orang-orang yang beriman
+                    di antara kalian) karena ketaatannya dalam hal tersebut (dan) Dia meninggikan pula (orang-orang yang
+                    diberi ilmu pengetahuan beberapa derajat) di surga nanti. (Dan Allah Maha Mengetahui apa yang kalian
+                    kerjakan). ">Tafsir</button>
+                <button class="tatbhiq-button" data-title="Tatbhiq Surah Al-Mujadilah, Ayat 11"
+                    data-text=" Faedah yang dapat diambil dari ayat ini adalah sebagai berikut: ayat Al-Qur’an tersebut menekankan pentingnya kepatuhan dan ketaatan terhadap perintah Allah dan pemimpin dalam konteks sosial dan agama. Ketaatan ini menunjukkan bahwa kepatuhan terhadap aturan adalah kunci untuk mencapai keharmonisan dalam kehidupan bermasyarakat dan beribadah.
+
+                    Selain itu, perintah untuk memberi kelapangan di dalam majelis mengajarkan sikap menghormati dan mendahulukan orang lain. Sikap ini mencerminkan rendah hati dan kepedulian terhadap sesama, serta mendorong terciptanya lingkungan yang inklusif dan ramah.
+                    
+                    Allah juga menjanjikan peningkatan derajat bagi orang yang taat dan beriman, baik di dunia maupun di akhirat. Ini juga berlaku bagi mereka yang berilmu, menekankan pentingnya mencari dan mengamalkan ilmu sebagai perbuatan yang sangat dianjurkan dalam Islam.
+                    
+                    Perintah untuk memberi kelapangan di majelis menunjukkan betapa pentingnya majelis ilmu sebagai tempat ilmu disebarkan dan dipelajari. Setiap kesempatan untuk belajar harus dimanfaatkan dengan sebaik-baiknya.
+                    
+                    Ayat ini juga mengingatkan bahwa Allah Maha Melihat dan Maha Mengetahui apa yang kita kerjakan. Ini mendorong setiap mukmin untuk selalu berbuat baik dan taat karena semua perbuatan kita akan diperhitungkan oleh Allah.
+                    
+                    
+                    
+                    
+                    
+                    ">Tatbhiq</button>
+            </div>
 
         </div>
 
     </section>
-    <!-- end of dalil container -->
+
+    <div id="popup-container">
+        <div id="popup-content">
+            <button id="close-popup">&times;</button>
+            <h1 id="popup-title"></h1>
+            <hr style="color: black; height: 3px; margin-bottom: 35px;">
+            <p id="popup-text"></p>
+        </div>
+    </div>
+    <!-- end of dalil al-quran section -->
+
 
     <!-- tafsir -->
     <section class="tafsir">
 
-        <div class="tafsi-container">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1"
+                d="
+                                                M0,160L60,160C120,160,240,160,360,181.3C480,203,600,245,720,229.3C840,213,960,139,1080,112C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+            </path>
+        </svg>
+        <br><br><br><br>
+        <h2>Dalil Hadits & Tafsirnya</h2>
+        <hr>
+        <div class="tafsir-container">
 
         </div>
 
     </section>
     <!-- end of dalil container -->
+
 
     <!-- dalil -->
     <section class="video">
@@ -45,5 +150,5 @@
     </section>
     <!-- end of dalil container -->
 
-    <script src="script.js"></script>
+    <script src="{{ asset('js/isso.js') }}"></script>
 @endsection
